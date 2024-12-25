d29bf5d12497a0316a397376f107c9d768bbef95
LeTienQuyet <21520428@gm.uit.edu.vn>
2024-12-25 15:55:08 +0700

    set ias=True, add 1 block, change dropout rate and latent dim

--- src/encoder.py
+++ src/encoder.py
@@ -6,13 +6,13 @@ class Encoder_Block(nn.Module):
         super(Encoder_Block, self).__init__()
         self.conv1 = nn.Conv2d(
             in_channels=in_channels, out_channels=int(out_channels/2),
-            kernel_size=kernel_size, stride=int(stride/2),
-            padding=padding, bias=False
+            kernel_size=kernel_size, stride=stride,
+            padding=padding, bias=True
         )
         self.conv2 = nn.Conv2d(
             in_channels=int(out_channels/2), out_channels=out_channels,
             kernel_size=kernel_size, stride=stride,
-            padding=padding, bias=False
+            padding=padding, bias=True
         )
         self.batch_norm = nn.BatchNorm2d(
             num_features=out_channels,
@@ -37,52 +37,55 @@ class Encoder(nn.Module):
         super(Encoder, self).__init__()
         self.encoder_block_1 = Encoder_Block(
             in_channels=3, out_channels=8,
-            kernel_size=3, stride=2, padding=1
+            kernel_size=3, stride=1, padding=1
         )
         self.encoder_block_2 = Encoder_Block(
             in_channels=8, out_channels=32,
-            kernel_size=3, stride=2, padding=1
+            kernel_size=3, stride=1, padding=1
         )
         self.encoder_block_3 = Encoder_Block(
             in_channels=32, out_channels=128,
-            kernel_size=3, stride=2, padding=1
+            kernel_size=3, stride=1, padding=1
+        )
+        self.encoder_block_4 = Encoder_Block(
+            in_channels=128, out_channels=512,
+            kernel_size=3, stride=1, padding=1
         )
         self.pooling = nn.MaxPool2d(
             kernel_size=2,
             stride=2
         )
         self.linear_1 = nn.Linear(
-            in_features=3200,
-            out_features=256,
+            in_features=512*7*7,
+            out_features=4096,
         )
         self.linear_2 = nn.Linear(
-            in_features=256,
-            out_features=128,
+            in_features=4096,
+            out_features=1024,
         )
         self.mu = nn.Linear(
-            in_features=128,
+            in_features=1024,
             out_features=latent_dim
         )
         self.log_var = nn.Linear(
-            in_features=128,
+            in_features=1024,
             out_features=latent_dim
         )
-        self.dropout_1 = nn.Dropout(p=0.25)
-        self.dropout_2 = nn.Dropout(p=0.25)
-        self.dropout_3 = nn.Dropout(p=0.25)
+        self.dropout_1 = nn.Dropout(p=0.5)
+        self.dropout_2 = nn.Dropout(p=0.5)
 
     def forward(self, x):
-        "Image shape = (640, 640, 3)"
-        x = self.encoder_block_1(x) # -> (160, 160, 8)
-        x = self.encoder_block_2(x) # -> (40, 40, 32)
-        x = self.encoder_block_3(x) # -> (10, 10, 128)
-        x = self.pooling(x)         # -> (5, 5, 128)
-        x = x.view(x.size(0), -1)   # -> (3200)
-        x = self.dropout_1(x)       # -> (3200)
-        x = self.linear_1(x)        # -> (256)
-        x = self.dropout_2(x)       # -> (256)
-        x = self.linear_2(x)        # -> (128)
-        x = self.dropout_3(x)       # -> (128)
-        mu = self.mu(x)             # -> (latent_dim=64)
-        log_var = self.log_var(x)   # -> (latent_dim=64)
+        "Image shape = (224, 224, 3)"
+        x = self.encoder_block_1(x) # -> (112, 112, 8)
+        x = self.encoder_block_2(x) # -> (56, 56, 32)
+        x = self.encoder_block_3(x) # -> (28, 28, 128)
+        x = self.encoder_block_4(x) # -> (14, 14, 512)
+        x = self.pooling(x)         # -> (7, 7, 512)
+        x = x.view(x.size(0), -1)   # -> (25088)
+        x = self.linear_1(x)        # -> (4096)
+        x = self.dropout_1(x)       # -> (4096)
+        x = self.linear_2(x)        # -> (1024)
+        x = self.dropout_2(x)       # -> (1024)
+        mu = self.mu(x)             # -> (latent_dim=256)
+        log_var = self.log_var(x)   # -> (latent_dim=256)
         return mu, log_var

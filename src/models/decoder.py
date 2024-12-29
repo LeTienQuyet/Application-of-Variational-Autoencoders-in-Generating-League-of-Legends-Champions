fa3e737a397db04140f03a6e01479217cf9df909
LeTienQuyet <21520428@gm.uit.edu.vn>
2024-12-29 23:26:07 +0700

    add active function, process RGBA or RGB images

--- src/models/decoder.py
+++ src/models/decoder.py
@@ -26,7 +26,7 @@ class Decoder_Block(nn.Module):
         return x
 
 class Decoder(nn.Module):
-    def __init__(self, latent_dim):
+    def __init__(self, latent_dim=1024, num_channels=4):
         super(Decoder, self).__init__()
         self.linear_1 = nn.Linear(
             in_features=latent_dim,
@@ -36,6 +36,7 @@ class Decoder(nn.Module):
             in_features=4096,
             out_features=25088
         )
+        self.active_func = nn.ReLU(inplace=True)
         self.dropout = nn.Dropout(p=0.5)
         self.decoder_block_1 = Decoder_Block(
             in_channels=512, out_channels=256,
@@ -58,22 +59,25 @@ class Decoder(nn.Module):
             output_padding_1=1, output_padding_2=0
         )
         self.decoder_block_5 = Decoder_Block(
-            in_channels=32, out_channels=3,
+            in_channels=32, out_channels=num_channels,
             kernel_size=3, stride=2, padding=1,
             output_padding_1=1, output_padding_2=0
         )
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        "Sampling reparameterize shape = (latent_dim = 256)"
+        "Sampling reparameterize shape = (latent_dim = 1024)"
         x = self.linear_1(x)             # -> (4096)
+        x = self.active_func(x)          # -> (4096)
         x = self.dropout(x)              # -> (4096)
         x = self.linear_2(x)             # -> (25088)
+        x = self.active_func(x)          # -> (25088)
+        x = self.dropout(x)              # -> (25088)
         x = x.view(x.size(0), 512, 7, 7) # -> (7, 7, 512)
         x = self.decoder_block_1(x)      # -> (14, 14, 256)
         x = self.decoder_block_2(x)      # -> (28, 28, 128)
         x = self.decoder_block_3(x)      # -> (56, 56, 64)
         x = self.decoder_block_4(x)      # -> (112, 112, 32)
-        x = self.decoder_block_5(x)      # -> (224, 224, 3)
+        x = self.decoder_block_5(x)      # -> (224, 224, [4, 3])
         x = self.sigmoid(x)
         return x
